3723f82126e1b1db8d8d0ab6d4dc26490e4bbcb9
Karmaur <caperez28@unisalle.edu.co>
2026-03-06 19:55:26 -0500

    Code:cambio desde otra rama

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,4 +1,11 @@
-a = 20
-b = 30
-c = a + b 
-print(c)
+print("¡Hola! ¿Qué deporte te gusta?\n")
+deporte = input("Introduce un deporte (futbol, baloncesto, natacion): ")
+
+if deporte == "futbol":
+    print("\n¡El fútbol es el deporte más popular del mundo!")
+elif deporte == "baloncesto":
+    print("\n¡El baloncesto es un deporte muy dinámico!")
+elif deporte == "natacion":
+    print("\n¡La natación es excelente para la salud!")
+else:
+    print("\n¡Ese deporte también es interesante!")
--- pruebas.py
+++
@@ -1,3 +0,0 @@
-print("Hola mundo")
-
-print("este es otro cambio en mi github")
